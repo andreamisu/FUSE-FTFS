11fa5a6b635d10c28f222d21dd0ec2223618c04f
Andrea Misuraca <andrea.misuraca@tum.de>
2024-10-06 19:53:52 +0200

    code refactoring, cleaning

--- memfs.c
+++ memfs.c
@@ -18,15 +18,15 @@ struct inode {
 	size_t st_size;
 	bool root_child;
 	bool is_folder;
-	struct inode* parent;
-	struct inode* child;
-	struct inode* next;
-	struct inode* prev;
+	struct inode *parent;
+	struct inode *child;
+	struct inode *next;
+	struct inode *prev;
 } inode;
 
 struct inode root;
 
-char* DELIMITER = "/";
+char *DELIMITER = "/";
 
 struct inode* path_level_evaluation(char* path, struct inode* node){
 	struct inode* elm = node;
@@ -59,16 +59,13 @@ struct inode* path_evaluation(const char* path){
 	char path_eval[strlen(path)+1];
 	char* ptr;
 	strcpy(path_eval, path);
-	if (strcmp(path, "/") == 0){
-			return &root;
-	}
-		
-	else{
+	if (strcmp(path, "/") == 0) {
+		return &root;
+	} else {
 		struct inode* res;
 		ptr = strtok(path_eval, DELIMITER);
-		if(root.child==NULL){
+		if (root.child==NULL) {
 			return NULL;
-
 		}
 		res = &root;
 		do{
@@ -78,43 +75,34 @@ struct inode* path_evaluation(const char* path){
 			if(res==NULL)
 				return NULL;
 			ptr = strtok(NULL, DELIMITER);
-		}while(ptr != NULL);
+		} while(ptr != NULL);
 		return res;
 	}
 }
 
 static int _mkdir(const char *path, mode_t mode)
 {
-	struct inode* elm, *last_sibling = NULL;
-
-	char* name, * mount_path;
+	struct inode *elm, *last_sibling = NULL;
+	char *name, *mount_path;
 	int first_child = 0;
-
-	struct inode* newdir = (struct inode*)malloc(sizeof(struct inode));
-
+	struct inode *newdir = (struct inode*)malloc(sizeof(struct inode));
 	name = name_evaluation(path);
-
-	if(strlen(name)> 255)
+	if (strlen(name) > 255)
 		return -1;
 
 	mount_path = (char*)malloc(sizeof(char) * (strlen(path)+1));
 	strcpy(mount_path,path);
 	mount_path[strlen(path) - strlen(name)] = '\0';	
 	elm = path_evaluation(mount_path);
-
-
-	if(elm==NULL)
+	if(!elm)
 		return -ENOENT;
 
-	if(elm->child!=NULL){
+	if (elm->child) {
 		last_sibling = elm->child;
 		while(last_sibling->next!=NULL)
 			last_sibling = last_sibling->next;
-	}
-	else 
+	} else 
 		first_child = 1;
-	
-	
 
 	newdir->name = name;
 	newdir->st_mode = mode | S_IFDIR;
@@ -131,105 +119,84 @@ static int _mkdir(const char *path, mode_t mode)
 	else
 		elm->child = newdir;
 	elm->st_nlink++;
-	
 	free(mount_path);
 	return 0;
 }
 
 static int _getattr( const char *path, struct stat *st )
 {
-	struct inode* file;
+	struct inode *file;
 	file = path_evaluation(path);
+	if (!file) 
+		return -ENOENT
 
-	if(file != NULL)
-	{
-		st->st_uid = getuid();
-		st->st_gid = getgid(); 
-		st->st_atime = time( NULL );
-		st->st_mtime = time( NULL ); 
-		st->st_mode =  file->st_mode;
-		st->st_nlink = file->st_nlink;
-		if(file->is_folder != true)
-			st->st_size = file->st_size;
-	}
-	else 
-		return -ENOENT;
-	
+	st->st_uid = getuid();
+	st->st_gid = getgid(); 
+	st->st_atime = time( NULL );
+	st->st_mtime = time( NULL ); 
+	st->st_mode =  file->st_mode;
+	st->st_nlink = file->st_nlink;
+	if (!file->is_folder)
+		st->st_size = file->st_size;
 	return 0;
 }
 
 
 static int _readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
 {
-	struct inode* file;
-
+	struct inode *file;
 	file = path_evaluation(path);
-	if(file != NULL)
-	{
-		filler( buffer, ".", NULL, 0 ); // Current Directory
-		filler( buffer, "..", NULL, 0 ); // Parent Directory
-		struct inode aux;
-		if(file->child != NULL){
-			aux = *(file->child);
-			while(1){
-				filler( buffer, aux.name, NULL, 0 );
-				if(aux.next == NULL)
-					break;
-				aux = *(aux.next);
-			}
+	if (!file)
+		return -ENOENT;
+	filler( buffer, ".", NULL, 0 ); // Current Directory
+	filler( buffer, "..", NULL, 0 ); // Parent Directory
+	struct inode *aux;
+	if (file->child) {
+		aux = file->child;
+		while (true) {
+			filler( buffer, aux->name, NULL, 0 );
+			if(!aux->next)
+				break;
+			aux = aux->next;
 		}
 	}
-	else 
-		return -ENOENT;
-	
 	return 0;
 }
 
 static int _read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
 {
-	struct inode* elm;
+	struct inode *elm;
 	elm = path_evaluation(path);
-	if(elm==NULL)
+	if (!elm)
 		return -ENOENT;
 
     memcpy(buffer, elm->content + offset, size );
-		
 	return strlen( elm->content ) - offset;
 }
 
-static int _create(const char *path, mode_t mode,
-		      struct fuse_file_info *fi)
+static int _create(const char *path, mode_t mode, struct fuse_file_info *fi )
 {
-	struct inode* elm, *last_sibling = NULL;
-
-	int first_child=0;
-
-	char* name, * mount_path;
-
-	struct inode* newdir = (struct inode*)malloc(sizeof(struct inode));
-
+	struct inode *elm, *last_sibling = NULL;
+	int first_child = 0;
+	char *name, *mount_path;
+	struct inode *newdir = (struct inode*)malloc(sizeof(struct inode));
 	name = name_evaluation(path);
-
-	if(strlen(name)> 255)
+	if (strlen(name) > 255)
 		return -1;
 
 	mount_path = (char*)malloc(sizeof(char) * (strlen(path)+1));
 	strcpy(mount_path,path);
 	mount_path[strlen(path) - strlen(name)] = '\0';
-
-	
 	elm = path_evaluation(mount_path);
-	if(elm==NULL)
+	if (!elm)
 		return -ENOENT;
 
-	if(elm->child!=NULL){
+	if (elm->child) {
 		last_sibling = elm->child;
-		while(last_sibling->next!=NULL)
+		while (last_sibling->next)
 			last_sibling = last_sibling->next;
-	}
-	else 
+	} else 
 		first_child=1;
-
 
 	newdir->name = name;
 	newdir->st_mode = mode | S_IFREG;
@@ -242,26 +209,23 @@ static int _create(const char *path, mode_t mode,
 	newdir->child = NULL;
 	newdir->is_folder = false;
 
-
-	if(!first_child)
+	if (!first_child)
 		last_sibling->next = newdir;
 	else
 		elm->child = newdir;
 	elm->st_nlink++;
-
 	free(mount_path);
 	return 0;
 }
 
-static int _write(const char *path, const char *buf, size_t size, off_t offset,
-	     struct fuse_file_info *fi)
+static int _write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi )
 {
 	struct inode* elm;
 	elm = path_evaluation(path);
-	if(elm==NULL)
+	if (!elm)
 		return -ENOENT;
 
-	memcpy(elm->content+offset, buf, size);
+	memcpy(elm->content + offset, buf, size);
 	elm->st_size = strlen(elm->content);
 	return size;
 }
@@ -270,20 +234,18 @@ static int _write(const char *path, const char *buf, size_t size, off_t offset,
 static int _open(const char *path, struct fuse_file_info *fi)
 {
 	int fd, ret=0;
-
-	struct inode* file;
+	struct inode *file;
 	file = path_evaluation(path);
-	if(file == NULL || file->is_folder == true)
+	if(!file || file->is_folder)
 		return -ENOENT;
 
-	fd = memfd_create( file->name, 0 );
+	fd = memfd_create(file->name, 0);
 	if (fd == -1)
 		return -errno;
 
 	int content_size = strlen(file->content) == 0 ? 0 : strlen(file->content)-1;
 	write( fd, file->content, sizeof(char) * content_size);
 	lseek( fd, 0, SEEK_SET );
-
 	fi->fh = fd;
 	return 0;
 }
@@ -292,26 +254,23 @@ static int _open(const char *path, struct fuse_file_info *fi)
 static struct fuse_operations operations = {
     .getattr	= _getattr,
     .readdir	= _readdir,
-    .read	= _read,
-	.mkdir = _mkdir,
-	.write = _write,
-	.open = _open,
-	.create = _create,
-
+    .read	    = _read,
+	.mkdir      = _mkdir,
+	.write      = _write,
+	.open       = _open,
+	.create     = _create
 };
 
 int main( int argc, char *argv[] )
 {
-
-	root.name = strdup("/");
-	root.st_mode =  S_IFDIR | 0755; 
-	root.st_nlink = 2;
-	root.prev = NULL;
-	root.next = NULL;
+	root.name       = strdup("/");
+	root.st_mode    =  S_IFDIR | 0755; 
+	root.st_nlink   = 2;
+	root.prev       = NULL;
+	root.next       = NULL;
 	root.root_child = true;
-	root.parent = NULL;
-	root.child = NULL;
-	root.is_folder = true;
-	
+	root.parent     = NULL;
+	root.child      = NULL;
+	root.is_folder  = true;
 	return fuse_main( argc, argv, &operations, NULL );
 }
